f9aaa20350b1cf4ab5f0b2985333e12dd4d2d691
Brendan Cowan <bacowan@ualberta.ca>
2020-02-16 15:54:32 -0400

    map markers

--- front-end/src/MapPage.js
+++ front-end/src/MapPage.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import OlMap from "ol/Map";
 import OlView from "ol/View";
-import OlLayerTile from "ol/layer/Tile";
-import OlSourceOSM from "ol/source/OSM";
-import { fromLonLat } from 'ol/proj';
+import OlTileLayer from "ol/layer/Tile";
+import OlOsmSource from "ol/source/OSM";
+import OlFeature from "ol/Feature";
+import OlPoint from "ol/geom/Point";
+import OlVectorSource from "ol/source/Vector";
+import OlVectorLayer from "ol/layer/Vector";
+import { Style, Fill, Stroke } from 'ol/style';
+import { fromLonLat, toLonLat } from 'ol/proj';
+import { RegularShape } from 'ol/style';
 import './map.css'
 
 class MapPage extends Component {
@@ -24,18 +30,84 @@ class MapPage extends Component {
   }
 
   createCenteredMap(lat, lon) {
+    let currentLocation = fromLonLat([lon, lat]);
+
+    let youAreHere = new OlFeature({
+      geometry: new OlPoint(currentLocation),
+    });
+    var vectorSource = new OlVectorSource({
+      features: [youAreHere]
+    });
+    var markerVectorLayer = new OlVectorLayer({
+      source: vectorSource,
+      style: new Style({
+        image: new RegularShape({
+          fill: new Fill({color: 'red'}),
+          stroke: new Stroke({color: 'black', width: 2}),
+          points: 16,
+          radius: 5,
+          angle: Math.PI / 4
+        })
+      })
+    });
+
     this.olmap = new OlMap({
       target: this.mapRef.current,
       layers: [
-        new OlLayerTile({
-          source: new OlSourceOSM()
-        })
+        new OlTileLayer({
+          source: new OlOsmSource()
+        }),
+        markerVectorLayer
       ],
       view: new OlView({
-        center: fromLonLat([lon, lat]),
+        center: currentLocation,
         zoom: 15
       })
     });
+
+    this.olmap.on('moveend', this.onMoveEnd.bind(this));
+  }
+
+  async onMoveEnd(event) {
+    // TODO: This will spam the database way too hard since it calls the database
+    // any time the map view changes at all
+    let extent = this.olmap.getView().calculateExtent();
+    let centerMeters = [(extent[0] + extent[2])/2, (extent[1] + extent[3])/2];
+    let centerLonLat = toLonLat(centerMeters);
+    let radius = Math.sqrt(Math.pow(extent[2] - centerMeters[0], 2) + Math.pow(extent[3] - centerMeters[1], 2)) / 1000;
+
+    let self = this;
+    if (navigator.geolocation) {
+      let fetchurl = `http://localhost:8080/interest_points?lat=${centerLonLat[1]}&lon=${centerLonLat[0]}&distance=${radius}`;
+      let response = await fetch(fetchurl, {mode: 'cors'});
+      let asJson = await response.json();
+
+      // TODO: filter out the points that have already been added
+
+      for (let i = 0; i < asJson.length; i++) {
+        let point = new OlFeature({
+          geometry: new OlPoint(fromLonLat([asJson[i].lon, asJson[i].lat])),
+        });
+        var vectorSource = new OlVectorSource({
+          features: [point]
+        });
+        var markerVectorLayer = new OlVectorLayer({
+          source: vectorSource,
+          style: new Style({
+            image: new RegularShape({
+              fill: new Fill({color: 'red'}),
+              stroke: new Stroke({color: 'black', width: 2}),
+              points: 16,
+              radius: 5,
+              angle: Math.PI / 4
+            })
+          })
+        });
+        self.olmap.addLayer(markerVectorLayer);
+      }
+    } else {
+      // TODO: Error handling
+    }
   }
 
   render() {
